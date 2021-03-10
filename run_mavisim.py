d9eefc7e9718d2f1d9bdf833e631f4322e56c98f
jcranney <jac1616@hotmail.com>
2021-03-10 17:18:59 +1100

    utilise fact that fft has conjugate symmetry

--- run_mavisim.py
+++ run_mavisim.py
@@ -13,6 +13,7 @@ from astropy.io import fits, ascii
 from astropy.table import Table
 
 from src.mavisim.generate_image import TileGenerator
+from src.mavisim.generate_image import ImageGenerator
 
 # Project-specific/
 #sys.path.append('/src')
@@ -39,50 +40,11 @@ exp_time = 3600
 source = Source(ngc3201, exp_time, tt_amp_fac=1.0, static_dist=False, stat_amp=1.0).main()
 #self, mavis_src, exp_time, static_dist, tt_amp_fac, stat_amp
 
-array_size = 12000
-pixsize = 3.75e-3
-cropped_width_as = 30
+image_gen = ImageGenerator(array_width_pix=12000, pixsize=3.75e-3, source_list=source,
+                            psfs_file="src/e2epsfs/e2e_psfs.fits", gauss_width_pix=34)
+image_gen.main()
+image_final = image_gen.get_rebinned_cropped(rebin_factor=2,cropped_width_as=30.0)
 
-# x-position of bottom-left-corner of each pixel:
-xx = np.arange(array_size)*pixsize
-FoV = xx[-1]-xx[0]
-xx -= (FoV/2+pixsize/2)
-yy = xx.copy()
-full_image = np.zeros([xx.shape[0],yy.shape[0]])
-
-tile_gen = TileGenerator(source, "src/e2epsfs/e2e_psfs.fits", 34)
-
-thresh = 1e-7
-t_tot += time.time() - t1
-t1 = time.time()
-for ni in range(len(source)):
-    tile = tile_gen.get_tile(ni)
-    xstart = np.abs(xx-tile[1][0]).argmin()
-    ystart = np.abs(yy-tile[1][1]).argmin()
-    if np.abs(xx[xstart]-tile[1][0]) > thresh or np.abs(yy[ystart]-tile[1][1]) > thresh:
-        print(ni)
-        print(xx[xstart])
-        print(yy[ystart])
-    full_image[ystart:ystart+tile_gen.psf_width_pix,xstart:xstart+tile_gen.psf_width_pix] += tile[0]
-    dt = time.time()-t1
-    t_tot += dt
-    print(f"star: {ni+1:6d},  took: {dt:7.4f} sec,  eta {(len(source)-ni)*dt:7.1f} sec",end="\r")
-    t1 = time.time()
-print("\ndone")
-print(f"time:  {t_tot:0.2f} sec")
-print(f"stars: {ni+1:d}")
-
-
-xx_cropped_id = np.abs(xx)<=cropped_width_as/2
-xx_cropped = xx[xx_cropped_id]
-cropped_image = full_image[xx_cropped_id,:][:,xx_cropped_id]
-
-def rebin(arr, new_shape):
-    shape = (new_shape[0], arr.shape[0] // new_shape[0],
-             new_shape[1], arr.shape[1] // new_shape[1])
-    return arr.reshape(shape).mean(-1).mean(1)
-
-rebinned_image = rebin(cropped_image,np.array(cropped_image.shape)//2)
-extent = cropped_width_as*np.array([-0.5,0.5,-0.5,0.5])
-plt.matshow(np.log(rebinned_image), extent=extent)
+extent = 30*np.array([-0.5,0.5,-0.5,0.5])
+plt.matshow(np.log(image_final), extent=extent)
 
--- src/mavisim/generate_image.py
+++ src/mavisim/generate_image.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pyfftw
 import time
 from astropy.io import fits
 from copy import deepcopy
@@ -6,7 +7,7 @@ from copy import deepcopy
 class PSF:
     """PSF object class"""
     def __init__(self, fits_ext, padto, *, dtype=np.complex128):
-        self.fft_data = np.fft.fftshift(np.fft.fft2(fits_ext.data,s=padto)).astype(dtype)
+        self.fft_data = (np.fft.rfft2(fits_ext.data,s=padto)).astype(dtype)
         self.xpos     = fits_ext.header["YPOS"]
         self.ypos     = fits_ext.header["XPOS"]
         self.Lambda   = fits_ext.header["LAMBDA"]
@@ -17,7 +18,7 @@ class TileGenerator:
              dtype = np.complex128 ):
         
         self.dtype=dtype
-        
+
         # Parse source info:
         self.source = deepcopy(source_list["Gauss_Info"])
         
@@ -66,79 +67,89 @@ class TileGenerator:
         return psf_out
     
     def get_tile(self,index):
+        t1 = time.time()
         if self.init == True:
+            new_shape = [self.fourier_tile_dim[0],self.fourier_tile_dim[1]//2+1]
+            self.psf_array = np.zeros(new_shape,dtype=self.dtype)
             self.nx = self.fourier_tile_dim[0] # square image only
             self.T  = self.dtype(self.gauss_width_as + self.psf_width_as)
             self.Ts = self.dtype(self.T/self.nx)
-            self.uu,self.vv = np.meshgrid(
+            uu,vv = np.meshgrid(
                 np.linspace(0,1/self.Ts,self.nx+1,dtype=self.dtype)[:-1]-1/(2*self.Ts),
                 np.linspace(0,1/self.Ts,self.nx+1,dtype=self.dtype)[:-1]-1/(2*self.Ts)
                 )
-            self.fft_pos = np.c_[self.uu.flatten(),self.vv.flatten()].T
-            self.esp1,self.esp2 = self.optimize_star_kernel(self.source[0], self.fft_pos, self.nx, self.T)
+            uu = np.fft.fftshift(uu)
+            vv = np.fft.fftshift(vv)
+            #self.fft_pos = np.c_[uu.flatten(),vv.flatten()].T
+            self.fft_pos = np.c_[uu[:new_shape[0],:new_shape[1]].flatten(),
+                                 vv[:new_shape[0],:new_shape[1]].flatten()].T
+            self.optimize_star_kernel(self.source[0])
             self.init = False
+        t2 = time.time()
         star = self.source[index]
-        psf_array = np.zeros(self.fourier_tile_dim,dtype=self.dtype)
-        #star[1] *= 0.0
-        #star[1] += 0.49*self.pixsize
-        #print(f"Forcing position to {star[1]}\"")
-        self.get_effective_psf_fft(star, psf_array)
+        if self.dtype==np.complex64:
+            star = [x.astype(np.float32) for x in star]
+        self.psf_array *= 0.0
+        t3 = time.time()
+        self.get_effective_psf_fft(star, self.psf_array)
+        t4 = time.time()
         offset = (((((star[1] % self.pixsize)/self.pixsize)+0.5)%1)-1)*self.pixsize # this has to be easier
         star_pos = (self.psf_width_as+self.gauss_width_as)/2 * np.array([1.0,1.0]) + offset
         bottom_left_corner = star[1]-offset-self.psf_width_as/2 - 0.5*self.pixsize
-        psf_array *= star[0] * self.get_star_kernel_fft(star[2], star_pos, self.fft_pos, self.nx, self.T, self.esp1, self.esp2)
-        return (np.fft.fftshift(
-            np.fft.ifft2(
-                np.fft.fftshift(psf_array)
-            ).astype(self.dtype))).real[:self.psf_width_pix,:self.psf_width_pix],bottom_left_corner
+        t5 = time.time()
+        self.psf_array *= star[0] * self.get_star_kernel_fft(star[2], star_pos)
+        t6 = time.time()
+        out = (np.fft.fftshift(
+            np.fft.irfft2(
+                (self.psf_array)
+            ).astype(self.dtype))).real[:self.psf_width_pix,:self.psf_width_pix]
+        t7 = time.time()
+
+        return out, bottom_left_corner, {
+            #"init":t2-t1,
+            "clear":t3-t2,
+            "psf":t4-t3,
+            "prep":t5-t4,
+            "star":t6-t5,
+            "fft":t7-t6,
+        }
     
-    def get_star_kernel_fft(self, cov, mu, fft_pos, nx, T, esp1, esp2):
+    def get_star_kernel_fft(self, cov, mu):
         offset = 2*np.pi*1j*mu.flatten()
         gaussian_fft = 2*np.pi*np.sqrt(np.linalg.det(cov))*np.exp(
                 -2*(np.pi)**2*np.einsum(
                     "ij,ii,ij->j",
-                    fft_pos,
+                    self.fft_pos,
                     cov,
-                    fft_pos,
-                    optimize=esp1
+                    self.fft_pos,
+                    optimize=self.esp1
                 ) + np.einsum(
                     "ij,i->j",
-                    fft_pos,
+                    self.fft_pos,
                     -offset,
-                    optimize=esp2
-                )).reshape([nx,nx])
-        gaussian_fft *= nx**2/T**2
+                    optimize=self.esp2
+                )).reshape(self.psf_array.shape)
+        gaussian_fft *= self.nx**2/self.T**2
         # TODO brute force normalise. Was an error of about 0.2% last I checked.
         return gaussian_fft
     
-    def get_star_kernel_fft_new(self, cov, mu, fft_pos, nx, T, esp1, esp2):
-        offset = 2*np.pi*1j*mu.flatten()
-        gaussian_fft = 2*np.pi*np.sqrt(np.linalg.det(cov))*np.exp(
-                np.array([
-                    -2*(np.pi)**2 * x.T @ cov @ x - x.T @ offset for x in fft_pos.T
-                    ])).reshape([nx,nx])
-        gaussian_fft *= nx**2/T**2
-        # TODO brute force normalise. Was an error of about 0.2% last I checked.
-        return gaussian_fft
-    
-    def optimize_star_kernel(self, star, fft_pos, nx, T):
+    def optimize_star_kernel(self, star):
         cov = star[2]
         mu = star[1]
         offset = 2*np.pi*1j*mu.flatten()
-        esp1 = np.einsum_path(
+        self.esp1 = np.einsum_path(
                     "ij,ii,ij->j",
-                    fft_pos,
+                    self.fft_pos,
                     cov,
-                    fft_pos,
+                    self.fft_pos,
                     optimize="optimal"
-                )
-        esp2 = np.einsum_path(
+                )[0]
+        self.esp2 = np.einsum_path(
                     "ij,i->j",
-                    fft_pos,
+                    self.fft_pos,
                     -offset,
                     optimize="optimal"
-                )
-        return esp1[0],esp2[0]
+                )[0]
     
     def main(self): 
         image = self.get_tile()
@@ -149,170 +160,45 @@ class TileGenerator:
         
         self.image_resampled = rebin(image,np.array(image.shape)//2)
     
-    
 class ImageGenerator:
-    def __init__( self, source_list, psfs_file, image_extent, *,
-            debug_scale_factor: int = 1, dtype = np.complex128 ):
+    def __init__(self, array_width_pix, pixsize, source_list, psfs_file, gauss_width_pix):        
+        self.pixsize = pixsize
+        self.xx = np.arange(array_width_pix)*pixsize
+        self.fov = self.xx[-1]-self.xx[0]
+        self.xx -= (self.fov/2+self.pixsize/2)
+        self.full_image = np.zeros([self.xx.shape[0],self.xx.shape[0]])
+        self.tile_gen = TileGenerator(source_list, psfs_file, gauss_width_pix)
         
-        self.dtype=dtype
-
-        # Parse source info:
-        self.source = deepcopy(source_list["Gauss_Info"])
-        for ni in range(len(self.source)):
-            self.source[ni][1] /= debug_scale_factor
-        for ni in range(len(self.source)):
-            self.source[ni][0] = dtype(self.source[ni][0])
-            self.source[ni][1] = dtype(self.source[ni][1])
-            self.source[ni][2] = dtype(self.source[ni][2])
-        
-        # Define nominal image geometry:
-        self.pixsize               = 3.75e-3 # TODO read this from fits Primary HDU metadata
-        self.final_image_width_as  = (image_extent[1]-image_extent[0])/debug_scale_factor
-        self.final_image_width_pix = int(round(self.final_image_width_as/self.pixsize))
-        self.final_image_dim       = np.array([
-                self.final_image_width_pix,
-                self.final_image_width_pix
-            ])
-
-        # Load PSF file and define Fourier Image Geometry:
-        self.psf_file          = psfs_file
-        psfs_fits              = fits.open(psfs_file, lazy_load_hdus=True)
-        self.psf_pitch         = 3.0 # TODO read this from fits Primary HDU metadata
-        self.psf_pitch = self.psf_pitch/debug_scale_factor
-        self.psf_width_pix     = psfs_fits[1].data.shape[0]
-        self.psf_width_as      = self.psf_width_pix * self.pixsize
-        self.fourier_image_dim = self.final_image_dim + np.array(psfs_fits[1].data.shape)
-        
-        # Create PSF objects and do their FFTs
-        print("Doing PSF FFTs:")
-        self.psfs = []
-        for psf in psfs_fits[1:]:
-            self.psfs.append(PSF(psf, self.fourier_image_dim, debug_scale_factor=debug_scale_factor))
-        psfs_fits.close()
-        print("Done.                          ")
-        
-    def get_effective_psf_fft(self, star, psf_out):
-        # Takes in a single star object (list of [flux, pos, cov])
-        # and all PSFs, and returns the effective PSF of that star.
-        s_pos = star[1]
-        for psf in self.psfs:
-            gamma_check = \
-                (np.abs(psf.xpos-s_pos[0]) < self.psf_pitch) and \
-                (np.abs(psf.ypos-s_pos[1]) < self.psf_pitch)
-            if gamma_check:
-                gamma = (
-                            (1-np.abs(psf.xpos-s_pos[0])/self.psf_pitch) * \
-                            (1-np.abs(psf.ypos-s_pos[1])/self.psf_pitch)
-                         ).astype(psf.fft_data.dtype)
+    def main(self):
+        time_profile = None
+        print(" | ".join([f"{x:7s}" for x in self.tile_gen.get_tile(0)[2]]) + " ||  ETA")
+        for ni in range(len(self.tile_gen.source)):
+            tile = self.tile_gen.get_tile(ni)
+            xstart = np.abs(self.xx-tile[1][0]).argmin()
+            ystart = np.abs(self.xx-tile[1][1]).argmin()
+            self.full_image[ystart:ystart+self.tile_gen.psf_width_pix,
+                            xstart:xstart+self.tile_gen.psf_width_pix] += tile[0]
+            if time_profile is not None:
+                time_profile = {x:time_profile[x]+tile[2][x] for x in time_profile}
             else:
-                continue
-            psf_out += gamma*psf.fft_data
-            # print(f"{psf.xpos:0.5f} {psf.ypos:0.5f} {gamma.real:0.4f}")
-        return psf_out
-    
-    def get_image(self):
-        image_fft = np.zeros(self.fourier_image_dim,dtype=self.dtype)
-        psf_origin = self.dtype(np.array([1.0,1.0]) * (self.psf_width_as/2))
-        storage_array = image_fft.copy()
-        time_clearing = 0
-        time_star = 0
-        time_psf = 0
-        time_product = 0
-        nx = self.fourier_image_dim[0] # square image only
-        T  = self.dtype(self.final_image_width_as + self.psf_width_as)
-        Ts = self.dtype(T/nx)
-        uu,vv = np.meshgrid(
-                np.linspace(0,1/Ts,nx+1,dtype=self.dtype)[:-1]-1/(2*Ts),
-                np.linspace(0,1/Ts,nx+1,dtype=self.dtype)[:-1]-1/(2*Ts)
-                )
-        fft_pos = np.c_[uu.flatten(),vv.flatten()].T
-        esp1,esp2 = self.optimize_star_kernel(self.source[0], self.dtype(fft_pos), nx, T)
-        print("Building image from sources:")
-        for star in self.source:
-            print(f"flux: {star[0].real:5.3e}, x: {star[1][0].real:7.3f}\", y: {star[1][1].real:7.3f}\"", end="\r")
-            t1 = time.time()
-            storage_array *= 0.0
-            t2 = time.time()
-            x2 = self.get_effective_psf_fft(star, storage_array)
-            t3 = time.time()
-            star_pos = star[1] - psf_origin
-            x1 = star[0] * self.get_star_kernel_fft(star[2], star_pos, fft_pos, nx, T, esp1, esp2)
-            t4 = time.time()
-            image_fft += x1 * x2
-            t5 = time.time()
-    
-            time_clearing += t2-t1
-            time_psf      += t3-t2
-            time_star     += t4-t3
-            time_product  += t5-t4
-        print("Done.                                                 ")
-        print(f"time clearing : {time_clearing:f}")
-        print(f"time star     : {time_star:f}")
-        print(f"time psf      : {time_psf:f}")
-        print(f"time product  : {time_product:f}")
+                time_profile = tile[2]
+            eta = (len(self.tile_gen.source)-ni)*(sum(time_profile.values())/(ni+1))
+            print(" | ".join([f"{time_profile[x]/(ni+1):7.5f}" for x in time_profile]) + \
+                    f" || {eta:0.2f}          ",end="\r")
+        print("\ndone")
+        print("Totals:")
+        print(" | ".join([f"{x:7s}" for x in time_profile])+" || TOTAL")
+        total = sum(time_profile.values())
+        print(" | ".join([f"{time_profile[x]/(ni+1):7.5f}" for x in time_profile]) + f" || {total:0.2f}")
         
-        return image_fft
-    
-    def get_star_kernel_fft(self, cov, mu, fft_pos, nx, T, esp1, esp2):
-        offset = 2*np.pi*1j*mu.flatten()
-        gaussian_fft = 2*np.pi*np.sqrt(np.linalg.det(cov))*np.exp(
-                -2*(np.pi)**2*np.einsum(
-                    "ij,ii,ij->j",
-                    fft_pos,
-                    cov,
-                    fft_pos,
-                    optimize=esp1
-                ) + np.einsum(
-                    "ij,i->j",
-                    fft_pos,
-                    -offset,
-                    optimize=esp2
-                )).reshape([nx,nx])
-        gaussian_fft *= nx**2/T**2
-        # TODO brute force normalise. Was an error of about 0.2% last I checked.
-        return gaussian_fft
-    
-    def get_star_kernel_fft_new(self, cov, mu, fft_pos, nx, T, esp1, esp2):
-        offset = 2*np.pi*1j*mu.flatten()
-        gaussian_fft = 2*np.pi*np.sqrt(np.linalg.det(cov))*np.exp(
-                np.array([
-                    -2*(np.pi)**2 * x.T @ cov @ x - x.T @ offset for x in fft_pos.T
-                    ])).reshape([nx,nx])
-        gaussian_fft *= nx**2/T**2
-        # TODO brute force normalise. Was an error of about 0.2% last I checked.
-        return gaussian_fft
-    
-    def optimize_star_kernel(self, star, fft_pos, nx, T):
-        cov = star[2]
-        mu = star[1]
-        offset = 2*np.pi*1j*mu.flatten()
-        esp1 = np.einsum_path(
-                    "ij,ii,ij->j",
-                    fft_pos,
-                    cov,
-                    fft_pos,
-                    optimize="optimal"
-                )
-        esp2 = np.einsum_path(
-                    "ij,i->j",
-                    fft_pos,
-                    -offset,
-                    optimize="optimal"
-                )
-        return esp1[0],esp2[0]
-    
-    def main(self): 
-        image = self.get_image()
-        image_cropped = (np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(image)).astype(self.dtype))).real[
-                self.psf_width_pix//2:-self.psf_width_pix//2,
-                self.psf_width_pix//2:-self.psf_width_pix//2
-                ]
-        
-        def rebin(arr, new_shape):
-            shape = (new_shape[0], arr.shape[0] // new_shape[0],
-                     new_shape[1], arr.shape[1] // new_shape[1])
-            return arr.reshape(shape).mean(-1).mean(1)
-        
-        self.image_resampled = rebin(image_cropped,self.final_image_dim//2)
-    
-    
+    def get_rebinned_cropped(self,rebin_factor,cropped_width_as):
+        xx_cropped_id = np.abs(self.xx)<=cropped_width_as/2
+        xx_cropped = self.xx[xx_cropped_id]
+        cropped_im = self.full_image[xx_cropped_id,:][:,xx_cropped_id]
+        rebinned_im = self.rebin(cropped_im,np.array(cropped_im.shape)//2)
+        return rebinned_im
+
+    def rebin(self, arr, new_shape):
+        shape = (new_shape[0], arr.shape[0] // new_shape[0],
+                 new_shape[1], arr.shape[1] // new_shape[1])
+        return arr.reshape(shape).mean(-1).mean(1)
