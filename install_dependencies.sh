e7ea4baad963c115158f5204e927c40247840073
jcranney <c3162562@uon.edu.au>
2021-07-06 19:39:45 +1000

    setup stuff

--- install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-pip install --upgrade pip
-pip install -r requirements.txt
-pip install --use-feature=in-tree-build src/mavisim
--- run_mavisim.py
+++ run_mavisim.py
@@ -1,6 +1,7 @@
 # Standard
 import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
+import sys
 import os
 import time
 
@@ -11,31 +12,23 @@ t1 = time.time()
 from astropy.io import fits, ascii
 from astropy.table import Table
 
-cwd = os.getcwd()
-
-os.chdir(os.getcwd() + "/src")
-import mavisim.addnoise
-import mavisim.input_parameters as input_par
 from mavisim.generate_image import ImageGenerator
+
+import mavisim.input_parameters as input_par
 from mavisim.Source import Source
-from mavisim.InputCoo import InputCoo
 
-os.chdir(cwd)
-
+# Nbody input plus chosen exposure time
 ngc3201 = input_par.nbody_in
-exp_time = 10
+
+exp_time = 3600
 source = Source(ngc3201, exp_time, tt_amp_fac=1.0, static_dist=False, stat_amp=1.0).main()
-input_coo = InputCoo(source, trim_cat=True).main()
+#self, mavis_src, exp_time, static_dist, tt_amp_fac, stat_amp
 
 image_gen = ImageGenerator(array_width_pix=12000, pixsize=3.75e-3, source_list=source,
-                            psfs_file="src/e2epsfs/e2e_psfs_100s_lqg.fits", gauss_width_pix=34)
+                            psfs_file="src/e2epsfs/e2e_psfs.fits", gauss_width_pix=34)
 image_gen.main()
 image_final = image_gen.get_rebinned_cropped(rebin_factor=2,cropped_width_as=30.0)
 
 extent = 30*np.array([-0.5,0.5,-0.5,0.5])
+plt.matshow(np.log(image_final), extent=extent)
 
-image_allnoise = mavisim.addnoise.add_all_noise(image_final, source.meta["exp_time"])
-image_final_noise = np.array(image_allnoise, dtype="float32")
-hdu = fits.PrimaryHDU(image_final_noise)
-hdul = fits.HDUList([hdu])
-hdul.writeto("output_noisy.fits", overwrite=True)
