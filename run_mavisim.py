4b9ea52515c6d757bf8997e4c8284367f0f670fa
jcranney <c3162562@uon.edu.au>
2021-07-06 16:54:02 +1000

    tidying/dependency stuff

--- run_mavisim.py
+++ run_mavisim.py
@@ -1,7 +1,6 @@
 # Standard
 import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
-import sys
 import os
 import time
 
@@ -12,39 +11,31 @@ t1 = time.time()
 from astropy.io import fits, ascii
 from astropy.table import Table
 
-from src.mavisim.generate_image import TileGenerator
-from src.mavisim.generate_image import ImageGenerator
-
-# Project-specific/
-#sys.path.append('/src')
 cwd = os.getcwd()
+
 os.chdir(os.getcwd() + "/src")
-
-#import mavisim.rampup
-#import mavisim.addconstantsky
-#import mavissimim.addnoise
-#import mavissimim.trimimage
+import mavisim.addnoise
 import mavisim.input_parameters as input_par
-#from rebin import rebin
-
+from mavisim.generate_image import ImageGenerator
 from mavisim.Source import Source
-#from mavissimim.AOGaussGrid import AOGaussGrid
-#from mavissimim.SeeingGrid import SeeingGrid
 from mavisim.InputCoo import InputCoo
 
 os.chdir(cwd)
-# Nbody input plus chosen exposure time
-ngc3201 = input_par.nbody_in
 
-exp_time = 3600
+ngc3201 = input_par.nbody_in
+exp_time = 10
 source = Source(ngc3201, exp_time, tt_amp_fac=1.0, static_dist=False, stat_amp=1.0).main()
-#self, mavis_src, exp_time, static_dist, tt_amp_fac, stat_amp
+input_coo = InputCoo(source, trim_cat=True).main()
 
 image_gen = ImageGenerator(array_width_pix=12000, pixsize=3.75e-3, source_list=source,
-                            psfs_file="src/e2epsfs/e2e_psfs.fits", gauss_width_pix=34)
+                            psfs_file="src/e2epsfs/e2e_psfs_100s_lqg.fits", gauss_width_pix=34)
 image_gen.main()
 image_final = image_gen.get_rebinned_cropped(rebin_factor=2,cropped_width_as=30.0)
 
 extent = 30*np.array([-0.5,0.5,-0.5,0.5])
-plt.matshow(np.log(image_final), extent=extent)
 
+image_allnoise = mavisim.addnoise.add_all_noise(image_final, source.meta["exp_time"])
+image_final_noise = np.array(image_allnoise, dtype="float32")
+hdu = fits.PrimaryHDU(image_final_noise)
+hdul = fits.HDUList([hdu])
+hdul.writeto("output_noisy.fits", overwrite=True)
